96f4b1477911a0b515a8a12a97f4516cf66e5683
choonlog <choonklog@gmail.com>
2019-04-01 13:28:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import reduction as red
 
 # red.LetsReduce("file name", "desired attractor", One plus product -> True / All plus product -> False)
-red.LetsReduce("Cohen.txt", "00110010110000000000100001001011", True)
+red.LetsReduce("metastasis_influence_network.txt", "00110010110000000000100001001011", True)
