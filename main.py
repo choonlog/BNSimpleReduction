7f4ebfabc5b5f3c81647d362db225159da59d654
choonlog <choonklog@gmail.com>
2019-07-18 16:06:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import reduction as red
+import BNsimpleReduction as red
 
 # red.LetsReduce("file name", "desired attractor", One plus product -> True / All plus product -> False)
 red.LetsReduce("metastasis_influence_network.txt", "00110010110000000000100001001011", True)
